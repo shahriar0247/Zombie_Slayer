e72934b279a3f65041e856c39d8c5374435b7419
Ahmed Shahriar <47810596+shahriar0247@users.noreply.github.com>
2021-02-17 01:53:55 +0600

    movement done
    adding weapons ik now

--- Assets/All/scripts/player/player_controller.cs
+++ Assets/All/scripts/player/player_controller.cs
@@ -8,7 +8,11 @@ public class player_controller : MonoBehaviour
     public float speed = 2;
     float turn_smooth_velocity;
     public float turn_smooth_time = 1f;
+    float max_speed_wait = 0;
 
+    public float walk_speed;
+    public float run_speed;
+    public float sprint_speed;
 
     public Transform cam;
 
@@ -28,17 +32,22 @@ public class player_controller : MonoBehaviour
     {
 
         movement();
+        aiming();
     }
 
     void set_speed(float value)
     {
         if (speed < value)
         {
-            speed += 8 * Time.deltaTime;
+            speed += 13 * Time.deltaTime;
+          
         }
         else if (speed > value)
         {
-            speed -= 10 * Time.deltaTime;
+            speed -= 15 * Time.deltaTime;
+        }
+        if (anim.GetFloat("movement") != speed) { 
+            anim.SetFloat("movement", speed);
         }
     }
 
@@ -48,44 +57,47 @@ public class player_controller : MonoBehaviour
         float Vertical_Input = Input.GetAxis("Vertical");
         float Horizontal_Input = Input.GetAxis("Horizontal");
 
-        Debug.Log(Vertical_Input.ToString());
-            
+        if (turn_smooth_time != 0.1f)
+        {
+            turn_smooth_time = 0.1f;
+        }
 
+       if (speed >= 0)
+        {
+            anim.SetBool("moving", true);
+        }
+        else
+        {
+            anim.SetBool("moving", false);
+        }
         if (Vertical_Input != 0 || Horizontal_Input != 0)
         {
+            anim.SetInteger("state", 1);
             if (Input.GetButton("Sprint"))
             {
-                set_speed(5.5f);
-                if (anim.GetInteger("state") != 3)
-                {
+               
+                  set_speed(sprint_speed);
 
-                    anim.SetInteger("state", 3);
-                }
+             
+
             }
 
             else if (Input.GetButton("Walk"))
             {
-                set_speed(1.7f);
-                if (anim.GetInteger("state") != 2)
-                {
-
-                    anim.SetInteger("state", 2);
-                }
+                set_speed(walk_speed);
+             
             }
             else
             {
 
-                set_speed(4.2f);
+                set_speed(run_speed);
 
-                if (anim.GetInteger("state") != 1)
-                {
-
-                    anim.SetInteger("state", 1);
-                }
+             
             }
         }
         else if (Input.GetButton("Vertical") == false && Input.GetButton("Horizontal") == false)
         {
+            set_speed(0f);
             anim.SetInteger("state", 0);
         }
 
@@ -93,14 +105,27 @@ public class player_controller : MonoBehaviour
 
         Vector3 direction = new Vector3(Horizontal_Input, 0, Vertical_Input).normalized;
 
-        if (direction.magnitude >= 0.1f)
-        {
+    
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turn_smooth_velocity, turn_smooth_time);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 movedir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(movedir.normalized * speed * Time.deltaTime);
+        
+    }
+
+    void aiming()
+    {
+        if (turn_smooth_time != 0.05f)
+        {
+            turn_smooth_time = 0.05f;
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            anim.SetInteger("state", 2);
         }
     }
+
 }
